c238096918f0a4afd9da3c6e7e78b98b91e3b0ad
Admin <admin@AlanPacheco.local>
2024-06-14 22:58:40 -0700

    changes in text size of button and color hero

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="button-bezel primary-color text-sm text-center py-2 px-6 rounded-lg button-shadow transition-colors duration-500 ease-in-out"
+    class="button-bezel primary-color text-center py-2 px-6 rounded-lg button-shadow transition-colors duration-500 ease-in-out"
     style=""
   >
     <slot />
