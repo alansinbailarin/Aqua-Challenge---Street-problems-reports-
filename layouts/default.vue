00c9b9f285391eaba5df668494699ee517d8980e
Admin <admin@AlanPacheco.local>
2024-07-02 12:19:38 -0700

    fixing sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,29 @@
 <template>
   <div style="font-family: 'Poppins'">
     <Navbar />
+    <MobileSidebar
+      :openSidebar="openSidebar"
+      @close-sidebar="closeMobileSidebar"
+    />
     <slot />
   </div>
 </template>
 <script setup>
 import { initFlowbite } from "flowbite";
 
+const openSidebar = ref(false);
+
 onMounted(() => {
   initFlowbite();
 });
+
+const toggleSidebar = () => {
+  openSidebar.value = !openSidebar.value;
+};
+
+const closeMobileSidebar = () => {
+  openSidebar.value = false;
+};
 </script>
 
 <style></style>
